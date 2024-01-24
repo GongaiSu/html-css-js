30f18b76a12bc26202a5b13dab4459aaf07b3532
gyf <2533865444@qq.com>
2024-01-25 07:31:48 +0800

    (家提交)24年1月24日

---
+++ vue代码/04_learn_composition/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <h2>{{ counter }}</h2>
+  <button @click="increment">+1</button>
+  <button @click="decrement">-1</button>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup(){
+    let counter = ref(10)
+    const increment =()=>{
+      console.log(counter)
+      console.log("increment")
+      counter.value++
+    }
+    const decrement =()=>{
+      counter.value--
+    }
+
+    return{
+      counter,
+      increment,
+      decrement
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
